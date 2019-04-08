97c8a61ec9679ed0997ff236f99fba7ebd6f9398
3w <45708498+xiwang9102@users.noreply.github.com>
2019-04-08 11:03:26 +0800

    Update config2.js

--- config2.js
+++ config2.js
@@ -1,2 +1,2 @@
  let apiToken2='MjY3NGVkMTJhYzljYzJkN2U1ZWI2NjI4YzQ1ODAxMjkwMDk5NGFiNgp=='
- let gistId2='MzViMjZjZDJiYWUyOTcwODkzY2ZlYjkwNzA5N2ExYzk='
+ let gistId2='MzViMjZjZDJiYWUyOTcwODkzY2ZlYjkwNzA5N2ExYazk='
